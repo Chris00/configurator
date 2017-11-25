01c68300b53582e773518b5e69d6db7802122c04
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2017-11-25 14:40:16 +0100

    Wrap documentation comments to 80 chars

--- src/configurator.mli
+++ src/configurator.mli
@@ -9,12 +9,14 @@ val create
   -> string (** name, such as library name *)
   -> t
 
-(** Return the value associated to a variable in the output of [ocamlc -config] *)
+(** Return the value associated to a variable in the output of [ocamlc
+   -config] *)
 val ocaml_config_var     : t -> string -> string option
 val ocaml_config_var_exn : t -> string -> string
 
-(** [c_test t ?c_flags ?link_flags c_code] try to compile and link the C code given in
-    [c_code]. Return whether compilation was successful. *)
+(** [c_test t ?c_flags ?link_flags c_code] try to compile and link the
+   C code given in [c_code]. Return whether compilation was
+   successful. *)
 val c_test
   :  t
   -> ?c_flags:   string list (** default: [] *)
@@ -42,8 +44,10 @@ module C_define : sig
   (** Import some #define from the given header files. For instance:
 
       {[
-        # C.C_define.import c ~includes:"caml/config.h" ["ARCH_SIXTYFOUR", Switch];;
-        - (string * Configurator.C_define.Value.t) list = ["ARCH_SIXTYFOUR", Switch true]
+        # Configurator.C_define.import c ~includes:"caml/config.h"
+                                       ["ARCH_SIXTYFOUR", Switch];;
+        - (string * Configurator.C_define.Value.t) list
+            = ["ARCH_SIXTYFOUR", Switch true]
       ]}
   *)
   val import
@@ -54,8 +58,8 @@ module C_define : sig
     -> (string * Type.t ) list
     -> (string * Value.t) list
 
-  (** Generate a C header file containing the following #define. [protection_var] is used
-      to enclose the file with:
+  (** Generate a C header file containing the following #define.
+      [protection_var] is used to enclose the file with:
 
       {[
         #ifndef BLAH
@@ -64,8 +68,8 @@ module C_define : sig
         #endif
       ]}
 
-      If not specified, it is inferred from the name given to [create] and the
-      filename. *)
+      If not specified, it is inferred from the name given to [create]
+      and the filename. *)
   val gen_header_file
     :  t
     -> fname:string
@@ -96,6 +100,6 @@ val main
   -> (t -> unit)
   -> unit
 
-(** Abort execution. If raised from within [main], the argument of [die] is printed as
-    [Error: <message>]. *)
+(** Abort execution. If raised from within [main], the argument of
+   [die] is printed as [Error: <message>]. *)
 val die : ('a, unit, string, 'b) format4 -> 'a
